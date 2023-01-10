edbf6a331a020e7ca4bbb1167086461ea44cecc1
Diana Pimenova <dpimenova@sigma-it.ru>
2023-01-10 10:48:49 +0300

    LKK-7 Created networks request for user and its models

--- src/app/store/global-state.ts
+++ src/app/store/global-state.ts
@@ -1,12 +1,14 @@
 import { Post } from "../post/models/post";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "../users/models/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Store {
   private posts = new BehaviorSubject<Post[]>([]);
+  private users = new BehaviorSubject<User[]>([]);
 
   setPosts(posts: Post[]): void {
     this.posts.next(posts);
@@ -14,5 +16,13 @@ export class Store {
 
   get posts$(): Observable<Post[]> {
     return this.posts;
+  }
+
+  setUsers(users: User[]) {
+    this.users.next(users);
+  }
+
+  get users$(): Observable<User[]> {
+    return this.users;
   }
 }
