dbe8217b8d2128658b273e548113dcf784f8caef
Diana Pimenova <dpimenova@sigma-it.ru>
2023-01-13 10:49:14 +0300

    LKK-10 Added comments

--- src/app/post/services/posts.service.ts
+++ src/app/post/services/posts.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs";
+import { forkJoin, map } from "rxjs";
 import { Post } from "../models/post";
 import { Store } from "../../store/global-state";
+import { Comment } from "../models/comment";
 
 @Injectable()
 export class PostsService {
@@ -17,6 +18,22 @@ export class PostsService {
       .pipe(
         map(response => {
           this.store.setPosts(response);
+        })
+      ).subscribe();
+  }
+
+  fetchPost(postId: number): void {
+    const urlPost = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    const urlComments = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+
+    forkJoin([
+      this.http.get<Post>(urlPost),
+      this.http.get<Comment[]>(urlComments)
+    ])
+      .pipe(
+        map(([responsePost, responseComments]) => {
+          this.store.setPost(responsePost);
+          this.store.setComments(responseComments);
         })
       ).subscribe();
   }
